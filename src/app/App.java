9d3d3ea5bfbb8cba0868a0c3883bc10b103ef3dd
Sebastian Rivera Soto <sebastianrs72@gmail.com>
2020-03-16 19:51:13 -0600

    Add configurations for JavaFX

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,7 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         new Simulation().start();
     }
 }
